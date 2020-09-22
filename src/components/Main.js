542104125f904e3b4bdee89eee63aba010e00662
Paukova <viktorija.paukova@accenture.com>
2020-09-22 13:38:38 +0300

    Beginning Components BrowserRouter installed

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import SignIn from "./SignIn"
+
+class Main extends Component {
+  render() {
+    return <div> Hello!</div>
+  }
+}
+
+export default Main
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import Main from "./components/Main"
+import { BrowserRouter } from "react-router-dom"
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>,
+  document.getElementById("root")
+)
