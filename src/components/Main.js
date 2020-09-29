13af4374be79650d2e4352c16257b0d7b8f3876d
Victoria <vicpa34@yahoo.com>
2020-09-29 13:14:04 +0300

    created private routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,16 @@ import SignUp from "./SignUp"
 import "../styles/stylesheet.css"
 import SignInHeader from "./SignInHeader"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Authentication from "./Authentication"
+import Private from "./Private"
 
 class Main extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route {...props} path="/private" render={props => (isAuthenticated() ? <Private /> : <Redirect to="/" />)} />
+
           <Route path="/" exact>
             <SignIn />
           </Route>
